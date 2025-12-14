e1dc5d267e802ec7833cfcfb53b9e85066116791
neiht-19 <thienphan23160@gmail.com>
2025-12-14 09:43:20 +0700

    HandleGroupInvite

--- LanChat/ServerLogConsole/obj/Debug/net6.0-windows/ServerLogConsole.AssemblyInfo.cs
+++ LanChat/ServerLogConsole/obj/Debug/net6.0-windows/ServerLogConsole.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerLogConsole")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45d0509978a0b5d4cc1a5793c9326da5a4c421f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c82989b4460222063a08f9f413e46a83ec382ad2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerLogConsole")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerLogConsole")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
