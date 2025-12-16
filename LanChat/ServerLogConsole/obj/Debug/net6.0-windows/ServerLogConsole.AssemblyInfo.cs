2d575044408645992c77d7ae12e6544ba2bdfe57
LcChinh21 <leducchinhdz@gmail.com>
2025-12-16 20:37:56 +0700

    commit

--- LanChat/ServerLogConsole/obj/Debug/net6.0-windows/ServerLogConsole.AssemblyInfo.cs
+++ LanChat/ServerLogConsole/obj/Debug/net6.0-windows/ServerLogConsole.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerLogConsole")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2513e04781872f44ff506569934bf69704984ab3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7303249d96b3b6a0e24ca4f9f1ccc629f1eb2d54")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerLogConsole")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerLogConsole")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
