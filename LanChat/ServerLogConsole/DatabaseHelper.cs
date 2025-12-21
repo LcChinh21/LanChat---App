cf3af622fce7960a1e3c0b4324142d7d3a41d923
LcChinh21 <leducchinhdz@gmail.com>
2025-12-21 17:23:44 +0700

    SUA LAI DB

--- LanChat/ServerLogConsole/DatabaseHelper.cs
+++ LanChat/ServerLogConsole/DatabaseHelper.cs
@@ -21,7 +21,7 @@ namespace ServerLogConsole
                 "Server=lanchat-mysql-lanchat.k.aivencloud.com;" +
                 "Database=LanChatDB;" +
                 "User=avnadmin;" +
-                "Password=;" +
+
                 "Port=15478;";
 
             _masterConnectionString = _connectionString;
